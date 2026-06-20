3bcc0ab7176610786ace67d0fe64967c169d9869
gautamsaurabh2710 <gautamsaurabh90@gmail.com>
2026-06-20 22:06:18 +0530

    change in mailer.go file

--- go-backend/internal/mailer/mailer.go
+++ go-backend/internal/mailer/mailer.go
@@ -8,18 +8,21 @@ import (
 
 func SendOTP(email, otp, user, pass string) error {
 
-	fmt.Println("EMAIL_USER:", user)
-    fmt.Println("EMAIL_PASS_SET:", pass != "")
-	if user == "" || pass == "" {
-		return fmt.Errorf("missing EMAIL_USER/EMAIL_PASS")
-	}
+	//fmt.Println("EMAIL_USER:", user)
+    //fmt.Println("EMAIL_PASS_SET:", pass != "")
+	//if user == "" || pass == "" {
+	//	return fmt.Errorf("missing EMAIL_USER/EMAIL_PASS")
+	//}
 
-	message := gomail.NewMessage()
-	message.SetHeader("From", user)
-	message.SetHeader("To", email)
-	message.SetHeader("Subject", "OTP Verification")
-	message.SetBody("text/plain", fmt.Sprintf("Your OTP is %s", otp))
+	//message := gomail.NewMessage()
+	//message.SetHeader("From", user)
+	//message.SetHeader("To", email)
+	//message.SetHeader("Subject", "OTP Verification")
+	//message.SetBody("text/plain", fmt.Sprintf("Your OTP is %s", otp))
 
-	dialer := gomail.NewDialer("smtp.gmail.com", 587, user, pass)
-	return dialer.DialAndSend(message)
+	//dialer := gomail.NewDialer("smtp.gmail.com", 587, user, pass)
+	//return dialer.DialAndSend(message)
+
+	fmt.Println("OTP:", otp)
+	return nil
 }
